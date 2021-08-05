d40b14417221bab6525a0be4fa3daa1d8a566039
Deise Santana <dsds3@aluno.ifnmg.edu.br>
2021-08-05 15:28:06 -0300

    Listar Funcionando

--- project/frontend/app/billingCycle/billingCycleController.js
+++ project/frontend/app/billingCycle/billingCycleController.js
@@ -6,16 +6,25 @@ angular.module('primeiraApp').controller('BillingCycleCtrl', [
 
 function BillingCycleController($http, msgs) {
     const vm = this
+    const url = 'http://localhost:3003/api/billingCycles'
     
+    vm.refresh = function(){
+        $http.get(url).then(function(response){
+            vm.billingCycle = {}
+            vm.billingCycles = response.data
+        })      
+    }
+
     vm.create = function() {
-        const url = 'http://localhost:3003/api/billingCycles'
         $http.post(url, vm.billingCycle)
         .then(function(response) {
-            vm.billingCycle = {}
+            vm.refresh()
             msgs.addSuccess('Operação Realizada com Sucesso!')
         })
         .catch(function(response) {
             msgs.addError(response.data.errors)
         })
     }
+
+    vm.refresh()
 }
