5a80966fd38c178404a640f3cb9ff2e11e98aedf
Deise Santana <dsds3@aluno.ifnmg.edu.br>
2021-08-05 16:18:40 -0300

    Tudo Funcionando

--- project/frontend/app/billingCycle/billingCycleController.js
+++ project/frontend/app/billingCycle/billingCycleController.js
@@ -38,6 +38,18 @@ function BillingCycleController($http, msgs, tabs) {
         tabs.show(vm, {tabDelete: true})
     }
 
+    vm.update = function() {
+        const updateUrl = `${url}/${vm.billingCycle._id}`
+        $http.put(updateUrl, vm.billingCycle)
+            .then(function(response) {
+                vm.refresh()
+                msgs.addSuccess('Operação Realizada com Sucesso!')
+            })
+            .catch(function(response) {
+                msgs.addError(response.data.errors)
+            })
+    }
+
     vm.delete = function() {
         const deleteUrl = `${url}/${vm.billingCycle._id}`
         $http.delete(deleteUrl, vm.billingCycle)
